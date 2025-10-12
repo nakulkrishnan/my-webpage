beca0698793419f8451721645631dfac7efc90d0
nakulkrishnan <53857123+nakulkrishnan@users.noreply.github.com>
2025-10-12 14:17:04 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Example: Alert on page load (remove or customize)
+window.onload = function() {
+    alert("Welcome to my personal site!");
+};
+
+// Or add more, like form validation if you add a contact form later.
